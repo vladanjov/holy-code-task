99ea26cc429e2ada058743904796b0a27e36b140
Vladan <jovanovic.vladan92@gmail.com>
2026-02-17 15:06:08 +0100

    - added index to fsqId - SearchResultDao

--- app/src/main/java/com/vladan/holycodetask/core/database/entity/VenueSearchResultEntity.kt
+++ app/src/main/java/com/vladan/holycodetask/core/database/entity/VenueSearchResultEntity.kt
@@ -1,8 +1,13 @@
 package com.vladan.holycodetask.core.database.entity
 
 import androidx.room.Entity
+import androidx.room.Index
 
-@Entity(tableName = "search_results", primaryKeys = ["query", "fsqId"])
+@Entity(
+    tableName = "search_results",
+    primaryKeys = ["query", "fsqId"],
+    indices = [Index(value = ["fsqId"])],
+)
 data class VenueSearchResultEntity(
     val query: String,
     val fsqId: String,
